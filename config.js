3854cdb72e1dcf6cc74e0b0685f62deafc14480b
CodeArtist7071 <65782896+CodeArtist7071@users.noreply.github.com>
2026-06-23 21:46:21 +0530

    saving

---
+++ config.js
@@ -0,0 +1,4 @@
+window.NOTIFYX_ONESIGNAL_ID = 'os_v2_app_hf6zhpmigjgvnhddofpvwl5gkksify3umdhu3y5pua3gthmyx3546lw23fwht44bj36y36via6o3v665z3o4sjvwogn3dleuse7rgfq';
+window.NOTIFYX_SITE_NAME = 'sample-website';
+window.NOTIFYX_COLOR = '#6C63FF';
+window.NOTIFYX_POSITION = 'bottom-right';
